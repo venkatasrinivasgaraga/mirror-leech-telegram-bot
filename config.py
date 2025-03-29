44a2256b4bf7c487e0073331c6474a803f1a7420
Garaga venkata srinivas <104828717+venkatasrinivasgaraga@users.noreply.github.com>
2025-03-29 15:23:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ USER_SESSION_STRING = ""
 CMD_SUFFIX = ""
 AUTHORIZED_CHATS = ""
 SUDO_USERS = ""
-DATABASE_URL = ""
+DATABASE_URL = "mongodb+srv://sainallamilli17:VinsmokeSanji_Bot@cluster0.bvpmzt1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 STATUS_LIMIT = 4
 DEFAULT_UPLOAD = "rc"
 STATUS_UPDATE_INTERVAL = 15
