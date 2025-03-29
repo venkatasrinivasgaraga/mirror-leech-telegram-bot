6eddabe42cbf4d5806ce04e32ed3f231a9b3c5a3
Garaga venkata srinivas <104828717+venkatasrinivasgaraga@users.noreply.github.com>
2025-03-29 16:08:07 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -2,4 +2,5 @@ BOT_TOKEN=7922478547:AAG2fOp4cGUd4tc22mIvuFk_03Fkrr3QUPY
 OWNER_ID=959340250
 TELEGRAM_API=27727369
 TELEGRAM_HASH=1a6616b34f66ed256a8330ad9cb674ed
-DATABASE_URL = mongodb+srv://sainallamilli17:VinsmokeSanji_Bot@cluster0.bvpmzt1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+DATABASE_URL =mongodb+srv://sainallamilli17:VinsmokeSanji_Bot@cluster0.bvpmzt1.mongodb.net/?retryWrites=true&w=majority&ssl=false
+
